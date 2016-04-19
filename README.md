2670b8dcc280bd33ba8ae08448e58ab36fd9d9e9
hunterowens <owens.hunter@gmail.com>
2016-04-19 12:39:17 -0500

    starting to work on the requests stufff

--- README.md
+++ README.md
@@ -5,4 +5,7 @@ The Impact Lab has a channel in Slack, called #standup, where we post our daily 
 This bot exists to remind those who didn't post a standup message to do so at new each day. 
 
 ## Deploying 
-This code is designed to be deployed on AWS Lambda. You'll need to configure both a slack API client and incoming webhook for the bot to post to. 
+This code is designed to be deployed on AWS Lambda. You'll need to configure both a slack API client and incoming webhook for the bot to post to.
+
+## Testing
+You can test the bot by running `python main.py`
--- main.py
+++ main.py
@@ -45,19 +45,25 @@ from __future__ import print_function
 import boto3
 import json
 import logging
+import requests 
 
 from base64 import b64decode
 from urllib2 import Request, urlopen, URLError, HTTPError
 
-
+ENCRYPTED_SLACK_TOKEN = "CiBVhQSX5nYIrdq5ceR6CokUkwMNrVwB/19yzaVQE3VObRK6AQEBAgB4VYUEl+Z2CK3auXHkegqJFJMDDa1cAf9fcs2lUBN1Tm0AAACRMIGOBgkqhkiG9w0BBwaggYAwfgIBADB5BgkqhkiG9w0BBwEwHgYJYIZIAWUDBAEuMBEEDGgO/qyI+JCNrHKfoAIBEIBM69UzS2OeFrD0xTSMfZJumJX8hvhBejQ7vVPYQMZrRjY+Rax7HrHbA3SUXcznQcLipzUnqxKEf/6f9iD4CoWCmDHzT6dzpXhtBp8CwA=="
 ENCRYPTED_HOOK_URL = "CiBVhQSX5nYIrdq5ceR6CokUkwMNrVwB/19yzaVQE3VObRLQAQEBAgB4VYUEl+Z2CK3auXHkegqJFJMDDa1cAf9fcs2lUBN1Tm0AAACnMIGkBgkqhkiG9w0BBwaggZYwgZMCAQAwgY0GCSqGSIb3DQEHATAeBglghkgBZQMEAS4wEQQMd78Kw7APHJ8y5oCAAgEQgGD8tmHEj/KszBdqau89uWktLwp6Az3++0XOcILugA+SGUG5Oq+VC/m7opXHdQGJrr12HYrh4Cez8J6c6jC/hh+TeV8FX0szERtbweONYwRkYEjgQOK9a+is6BtVAlxDv2E="
 SLACK_CHANNEL = 'standup'  # Enter the Slack channel to send a message to
-
+SLACK_API_ROOT = "https://slack.com/api/"
 HOOK_URL = "https://" + boto3.client('kms').decrypt(CiphertextBlob=b64decode(ENCRYPTED_HOOK_URL))['Plaintext']
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
+def get_todays_msg():
+    token = boto3.client('kms').decrypt(CiphertextBlob=b64decode(ENCRYPTED_SLACK_TOKEN))['Plaintext']
+    r = requests.get(SLACK_API_ROOT + 'channels.info?' + 'channel=' + 
+                     SLACK_CHANNEL + '&token=' + token)
+    return r 
 
 def main():
     
